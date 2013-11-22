04d56e217c46fda83581af228094c760d4f54604
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-11-22 22:59:58 +0000

    Remove hardcoded values

--- copy_files.rb
+++ copy_files.rb
@@ -1,6 +1,7 @@
 require "FileUtils"
-files = Dir.entries("tweets").take(5003).drop(3)
+N = 10000
+files = Dir.entries("tweets").take(N+3).drop(3)
 
 files.each do |file|
-  FileUtils.cp "tweets/#{file}", "5000_tweets/#{file}"
+  FileUtils.cp "tweets/#{file}", "#{N}_tweets/#{file}"
 end
