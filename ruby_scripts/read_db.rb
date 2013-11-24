037b7464b02aa00943aa371168190167a288df25
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-11-24 13:57:07 +0000

    Minor modifications to db reader

--- ruby_scripts/read_db.rb
+++ ruby_scripts/read_db.rb
@@ -1,9 +1,13 @@
 require 'mongo'
 
-remoteCollection = Mongo::Connection.new("", 27017).db("tweets")["apple_tweets"]
+remoteCollection = Mongo::Connection.new("146.185.155.128", 27017).db("tweets")["apple_tweets"]
 localCollection = Mongo::Connection.new("localhost", 27017).db("tweets")["apple_tweets"]
 
-remoteCollection.find({}, {limit: 500000}).each do |doc|
-  localCollection.insert(doc)
+localCollection.find().each do |doc|
+  remoteCollection.insert(doc)
 end
 
+# remoteCollection.find({}, {limit: 500000}).each do |doc|
+#   localCollection.insert(doc)
+# end
+# 
