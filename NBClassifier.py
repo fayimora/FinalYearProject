339b5fe2a31ab2b4411f6251a06726eb693e27a2
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-01-12 19:58:28 +0000

    Give my initial classifier some meat :smile:

--- NBClassifier.py
+++ NBClassifier.py
@@ -1,10 +1,18 @@
 from nltk.classifier import NaiveBayesClassifier
+import glob
 
-# extract relevant and irrelevant examples
-relevant_examples = []
-irrelevant_examples = []
+def get_data(path):
+    examples = glob.glob(path)
+    to_text = lambda fname: open(fname).read()
+    return map(to_text, examples)
 
+print "Extracting relevant and irrelevant examples..."
+relevant_examples = get_data("data/relevant/*")
+irrelevant_examples = get_data("data/relevant/*")
+
+print "Creating training set..."
 training_set = relevant_examples + irrelevant_examples
 
+print "Training in progress..."
 classifier = NaiveBayesClassifier.train(training_set)
 
