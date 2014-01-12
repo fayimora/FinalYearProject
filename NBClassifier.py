3336489347fa0c456e0bb52fd45deb6e746ce959
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-01-12 22:13:32 +0000

    First main iteration of the naive bayes classifier

--- NBClassifier.py
+++ NBClassifier.py
@@ -1,18 +1,25 @@
-from nltk.classifier import NaiveBayesClassifier
+from nltk.classify import NaiveBayesClassifier
 import glob
 
-def get_data(path):
+def get_data(path, label):
     examples = glob.glob(path)
-    to_text = lambda fname: open(fname).read()
+    to_text = lambda fname: (features(open(fname).read()), label)
     return map(to_text, examples)
 
+def features(sentence):
+    words = sentence.lower().split()
+    return dict((w, True) for w in words)
+
 print "Extracting relevant and irrelevant examples..."
-relevant_examples = get_data("data/relevant/*")
-irrelevant_examples = get_data("data/relevant/*")
+relevant_examples = get_data("data/relevant/*", "relevant")
+irrelevant_examples = get_data("data/relevant/*", "irrelevant")
 
 print "Creating training set..."
 training_set = relevant_examples + irrelevant_examples
 
 print "Training in progress..."
 classifier = NaiveBayesClassifier.train(training_set)
+print "Finished training!"
+
+classifier.show_most_informative_features()
 
