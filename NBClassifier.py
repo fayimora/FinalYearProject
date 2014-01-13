e5fde391136d5519f230a578148b228d2e7f066e
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-01-13 13:30:39 -0500

    Refactor code and add more print statements

--- NBClassifier.py
+++ NBClassifier.py
@@ -1,3 +1,4 @@
+import nltk
 from nltk.classify import NaiveBayesClassifier
 import glob
 
@@ -15,11 +16,18 @@ relevant_examples = get_data("data/relevant/*", "relevant")
 irrelevant_examples = get_data("data/irrelevant/*", "irrelevant")
 
 print "Creating training set..."
-training_set = relevant_examples + irrelevant_examples
+featuresets = relevant_examples + irrelevant_examples
+print "Featuresets: " + str(len(featuresets))
+
+N = 100000
+train_set, test_set = featuresets[N:], featuresets[:N]
+print "Train set: " + str(len(train_set))
+print "Test set: " + str(len(test_set))
 
 print "Training in progress..."
-classifier = NaiveBayesClassifier.train(training_set)
+classifier = NaiveBayesClassifier.train(train_set)
 print "Finished training!"
 
 classifier.show_most_informative_features()
-
+accuracy = nltk.classify.util.accuracy(classifier, test_set)
+print "Accuracy: " + str(accuracy)
