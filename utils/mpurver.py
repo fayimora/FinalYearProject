97518d64729ee801a2e4920de48d37462badfadc
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-01-23 00:58:59 +0000

    Update mpurver.py

--- utils/mpurver.py
+++ utils/mpurver.py
@@ -2,10 +2,12 @@ import glob, os
 
 rel = glob.glob("data/relevant/*")[:1800]
 irr = glob.glob("data/irrelevant/*")[:1800]
+print "Done globbing"
 
 for f in rel:
-  os.system("cp " + f + " mpurver/relevant")
+  os.system("cp %s data_3600/relevant" % f)
+print "Done copying relevant files"
 
 for f in irr:
-  os.system("cp " +  f + " mpurver/irrelevant")
-
+  os.system("cp %s data_3600/irrelevant" % f)
+print "Done copying irrelevant files"
