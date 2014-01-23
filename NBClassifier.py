3a5423d68cf4634cae17b0399f67c764c627c7c4
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-01-23 20:30:32 +0000

    Major Updates :smile:
    * Use cross validation(ShuffleSplit) to test model
    * Convert all python arrays to numpy arrays for consistency
    * Change label names for compatibility. Use 1 to denote 'relevant' and -1 for 'irrelevant'
    * Compute accuracy, precision and the area under the curve of recall against precision

--- NBClassifier.py
+++ NBClassifier.py
@@ -1,45 +1,53 @@
-import glob
+import glob, random
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import cross_validation
+from sklearn.metrics import precision_recall_curve, auc
 import numpy as np
 
 def get_data(path, label):
-    print "Generating %s data and labels" % label
+    key = {1: 'relevant', -1: 'irrelevant'}
+    print "Generating %s's data and labels" % key[label]
     examples = glob.glob(path)
-    to_text = lambda fname: open(fname).read()
-    examples = map(to_text, examples)
-    labels = [label for t in examples]
-    return (examples, labels)
+    to_pair = lambda fname: (open(fname).read(), label)
+    examples = map(to_pair, examples)
+    random.shuffle(examples)
+
+    tweets, labels = [],[]
+    for t, l in examples:
+        tweets.append(t)
+        labels.append(l)
+    return (tweets, labels)
 
 print "Extracting relevant and irrelevant examples..."
-relevant_examples, relevant_labels = get_data("data/relevant/*", "relevant")
-irrelevant_examples, irrelevant_labels = get_data("data/irrelevant/*", "irrelevant")
+relevant_examples, relevant_labels = get_data("data/relevant/*", 1)
+irrelevant_examples, irrelevant_labels = get_data("data/irrelevant/*", -1)
 
 print "Creating training set..."
-train_data = relevant_examples + irrelevant_examples
-y_train = np.array(relevant_labels + irrelevant_labels)
-Y = np.array(relevant_labels + irrelevant_labels)
+train_data = np.asarray(relevant_examples + irrelevant_examples)
+Y = np.asarray(relevant_labels + irrelevant_labels)
+y_train = Y
 
-vectorizer = CountVectorizer(min_df=1, ngram_range=(1,3), stop_words='english')
-print "Generating features with", vectorizer
-X_train = vectorizer.fit_transform(train_data)
-print "X_train: n_samples: %d, n_features: %d" % X_train.shape
-print "y_train: n_samples: %d" % y_train.shape
-
-X_test = vectorizer.transform(train_data[1500:2100])
-y_test = y_train[1500:2100]
-print "X_test: n_samples: %d, n_features: %d" % X_test.shape
-print "y_test: n_samples: %d" % y_test.shape
-
+vectorizer = CountVectorizer(min_df=1, ngram_range=(1,3))
 clf = MultinomialNB()
-clf.fit(X_train, y_train)
 
-# from sklearn.metrics import f1_score
-# pred = clf.predict(X_test)
-# score = f1_score(y_test, pred)
-# print("f1-score:   %0.3f" % score)
+cv = cross_validation.ShuffleSplit(n=len(train_data), n_iter=10, test_size=0.20, indices=True,
+        random_state=0)
 
-accuracy = clf.score(X_test, y_test)
-print "Accuracy:", accuracy
-print()
+scores, pr_scores = [], []
+for train_idx, test_idx in cv:
+    X_train, y_train = train_data[train_idx], Y[train_idx]
+    X_test, y_test = train_data[test_idx], Y[test_idx]
+
+    clf.fit(vectorizer.fit_transform(X_train), y_train)
+
+    score = clf.score(vectorizer.transform(X_test), y_test)
+    scores.append(score)
+
+    proba = clf.predict_proba(vectorizer.transform(X_test))
+    precision, recall, pr_thresholds = precision_recall_curve(y_test, proba[:,1])
+    pr_scores.append(auc(recall, precision))
+    summary = (np.mean(scores), np.std(scores), np.mean(pr_scores), np.std(pr_scores))
+    print "%.4f\t%.4f\t%.4f\t%.4f" % summary
+
 
