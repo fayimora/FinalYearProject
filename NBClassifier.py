ae7aef080f6ab84f4707645e1ac305ebf9b9651d
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-03-16 18:54:11 +0000

    Add auc plot shenanigans

--- NBClassifier.py
+++ NBClassifier.py
@@ -1,4 +1,4 @@
-import glob, random, re, os
+import glob, random, re
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn import cross_validation
@@ -6,6 +6,7 @@ from sklearn.metrics import precision_recall_curve, auc, f1_score
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
 import numpy as np
+from matplotlib import pylab
 
 
 def rm_links(s):
@@ -66,6 +67,8 @@ def train_model(clf, X, Y):
     cv = cross_validation.ShuffleSplit(n=X.size, n_iter=10, test_size=0.20,
                                        indices=True, random_state=0)
 
+    print clf
+    print cv
     scores, pr_scores = [], []
     precisions, recalls, thresholds = [], [], []
 
@@ -91,6 +94,43 @@ def train_model(clf, X, Y):
                    np.std(pr_scores))
         print "%.4f\t%.4f\t%.4f\t%.4f" % summary
 
+    scores_to_sort = pr_scores
+    median = np.argsort(scores_to_sort)[len(scores_to_sort) / 2]
+    name = "NB Tweets Classifier"
+
+    plot_pr(pr_scores[median], name, "04", precisions[median], recalls[median],
+            label=name)
+
+
+def get_best_classifier():
+    classifier = MultinomialNB(alpha=0.01, class_prior=None, fit_prior=True)
+    vectorizer = TfidfVectorizer(analyzer='word', binary=True, charset=None,
+                                 charset_error=None, max_df=1.0, min_df=1,
+                                 ngram_range=(2, 2), norm='l2',
+                                 smooth_idf=True, stop_words=None,
+                                 sublinear_tf=False, use_idf=False)
+
+    clf = Pipeline([('vect', vectorizer), ('clf', classifier)])
+    return clf
+
+
+def plot_pr(auc_score, name, phase, precision, recall, label=None):
+    """This function plots the ROC curve"""
+    pylab.clf()
+    pylab.figure(num=None, figsize=(10, 6))
+    pylab.grid(True)
+    pylab.fill_between(recall, precision, alpha=0.5)
+    pylab.plot(recall, precision, lw=1)
+    pylab.xlim([0.0, 1.0])
+    pylab.ylim([0.0, 1.0])
+    pylab.xlabel('Recall')
+    pylab.ylabel('Precision')
+    pylab.title('P/R curve (AUC=%0.2f) / %s' % (auc_score, label))
+    filename = name.replace(" ", "_")
+    # pylab.show()
+    # pylab.savefig(os.path.join(".", "pr_%s_%s.png"%(filename, phase))
+    pylab.savefig("pr_%s_%s.png" % (filename, phase))
+
 
 if __name__ == '__main__':
     print "Extracting relevant and irrelevant examples..."
@@ -101,13 +141,15 @@ if __name__ == '__main__':
     X = np.asarray(relevant_examples + irrelevant_examples)
     y = np.asarray(relevant_labels + irrelevant_labels)
 
-    # vectorizer = CountVectorizer(min_df=1, ngram_range=(1, 2), stop_words='english')
-    vectorizer = TfidfVectorizer(min_df=1, ngram_range=(1, 2), stop_words='english')
+    # vectorizer = CountVectorizer(min_df=1, ngram_range=(1, 2))
+    vectorizer = CountVectorizer(min_df=1, ngram_range=(1, 2), stop_words='english')
+    # vectorizer = TfidfVectorizer(min_df=1, ngram_range=(1, 2), stop_words='english')
     classifier = MultinomialNB()
     clf = Pipeline([('vect', vectorizer), ('clf', classifier)])
 
     # train_model(clf, X, y)
-    best_clf = grid_search_model(clf, X, y)
+    train_model(get_best_classifier(), X, y)
+    # best_clf = grid_search_model(clf, X, y)
 
 
 # show_most_informative_features(vectorizer, classifier, n=40)
