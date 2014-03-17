611ce05f5e2b26e58e0ef83450a30c7a44500110
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-03-17 12:08:35 +0000

    Add script to help create instances

---
+++ utils/utils.py
@@ -0,0 +1,52 @@
+from pymongo import *
+import pickle, numpy
+
+def get_collection(db_name, coll):
+    print "Connecting to the database..."
+    client = MongoClient('146.185.155.128', 27017)
+    db = client[db_name]
+    collection = db[coll]
+    return collection
+
+
+def create_train_instances():
+    apple_tweets = get_collection('tweets', 'apple_tweets')
+    print "Fetching data from the database..."
+    labelled_data = apple_tweets.find({"relevant": {"$exists": True}})
+
+    print "Now creating instances..."
+    for tweet in labelled_data:
+        loc = "data/"
+        if(tweet['relevant']):
+            loc = loc + "relevant/" + str(tweet['_id'])
+        else:
+            loc = loc + "irrelevant/" + str(tweet['_id'])
+
+        f = open(loc, 'w')
+        f.write(tweet['text'].encode("utf-8"))
+        f.close()
+
+    print "Done creating instances!"
+
+
+def create_instances():
+    apple_tweets = get_collection('tweets', 'apple_tweets')
+    labelled_data = apple_tweets.find()
+
+    clf = pickle.load(open('nb_tweets_classifier.dat', 'r'))
+
+    print "Creating instances..."
+
+    for tweet in labelled_data:
+        loc = "tweets/"
+        loc = loc + str(tweet['_id'])
+        status = tweet['text'].encode("utf-8")
+        
+        print type(pred)
+        if pred == 1:
+            f = open(loc, 'w')
+            f.write(status)
+            f.close()
+
+if __name__ == '__main__':
+    create_instances()
