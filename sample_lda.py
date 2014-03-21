f68a45b88b6367e07eaeb1e84cd061b56da96cb5
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-03-21 02:21:15 +0000

    Cleanup sample lda

--- sample_lda.py
+++ sample_lda.py
@@ -1,7 +1,11 @@
-# from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+import glob, logging
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
 from gensim import corpora, models
-from gensim.models import ldamodel
+from gensim.models.ldamodel import LdaModel
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.INFO)
 
 
 def to_features(vect, doc):
@@ -19,15 +23,16 @@ documents = ['Human machine interface for lab abc computer applications',
              'Graph minors IV Widths of trees and well quasi ordering',
              'Graph minors A survey']
 
+files = glob.glob("data_3600/relevant/*")
+documents = map(lambda f: open(f).read(), files)
 
-# texts = [[word for word in document.lower().split() if word not in
-#           ENGLISH_STOP_WORDS] for document in documents]
-
-vectorizer = CountVectorizer(min_df=1, ngram_range=(1, 2), stop_words='english')
+stop_words = ENGLISH_STOP_WORDS.union(['iphone', 'ipod', 'ipad', 'http', 'rt', 'apple'])
+vectorizer = CountVectorizer(min_df=1, ngram_range=(1, 1), stop_words=stop_words)
 texts = [to_features(vectorizer, document) for document in documents]
 
 dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
-lda = ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=10)
+lda = LdaModel(corpus=corpus, id2word=dictionary, num_topics=50, passes=10, iterations=1000, update_every=10)
 corpus_lda = lda[corpus]
+preds = [l for l in corpus_lda]
