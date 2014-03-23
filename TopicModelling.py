f41180ecfcd6e02b42e9eae84cbf711165ac8fb4
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-03-23 00:43:55 +0000

    Account for tweets that contain only stop words

--- TopicModelling.py
+++ TopicModelling.py
@@ -10,16 +10,20 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
 
 
 def to_features(vect, doc):
-    vect.fit_transform([doc])
-    return vect.get_feature_names()
+    try:
+        vect.fit_transform([doc])
+        return vect.get_feature_names()
+    except ValueError, e:
+        return ['']
 
 print "Accumulating data..."
 files = glob.iglob("data_3600/relevant/*")
 documents = imap(lambda f: open(f).read(), files)
 
 print "Converting data to features..."
-stop_words = ENGLISH_STOP_WORDS.union(['iphone', 'ipod', 'ipad', 'mac', 'imac', 'http', 'https', 'rt', 'apple'])
-vectorizer = CountVectorizer(min_df=1, ngram_range=(1, 1), stop_words=stop_words)
+stop_words = ['iphone', 'ipod', 'ipad', 'mac', 'imac', 'http', 'https', 'rt', 'apple']
+stop_words = ENGLISH_STOP_WORDS.union(stop_words)
+vectorizer = CountVectorizer(min_df=1, ngram_range=(1, 2), stop_words=stop_words)
 texts = [to_features(vectorizer, document) for document in documents]
 
 print "Converting texts to bag of words..."
